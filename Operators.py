f49febcf862dd8343c0a70103cfe77166f0e3e2d
Piper Walker <72222334+pepper2272@users.noreply.github.com>
2020-10-08 11:24:16 -0700

    All the example programs from Session 2 and 3

---
+++ Operators.py
@@ -0,0 +1,36 @@
+x = 9 
+y = 3
+
+#Arithmetic Operators 
+print(x+y) #Addition
+print(x-y) #Subtraction
+print(x*y) #Multiplication
+print(x/y) #Division
+print(x%y) #Modulus
+print(x**y) #Eponentiation 
+x = 9.191823
+print(x//y) #Floor division
+
+#Assignment operators
+x = 9 #set x = 9
+x += 3 #x = x + 3
+print(x)
+x = 9
+x -= 3 #x= x - 3
+print(x)
+x*= 3 #x =x*3
+print(x)
+x /= 3 # x = x / 3
+print(x)
+x **= 3 #x = x ** 3
+print(x)
+
+#Comparison operators
+x = 9 
+y = 3
+print(x==y) #True if x equals y, False if otherwise
+print(x!=y) #True if x does not equal y, False if otherwise
+print(x>y)  #Ture if x is greater than y false otherwise
+print(x<y)  #True if x is less than y, false otherwise
+print(x>=y)   #True if x is less than or equal to y, False otherwise
+print(x<=y)  #True if x is less than or equal to y, False otherwise
---
+++ useful_modules.py
@@ -0,0 +1,8 @@
+import numpy as np #the Numpy library 
+import matplotlib.pyplot as plt #Matplotlib's pyplot
+
+import sys # gives acces to a C-like sys library
+import os #gives access to operating system
+
+print(sys.argv) #prints any command ling arguments, incl program name
+print(os.getcwd()) #print the current working directory
